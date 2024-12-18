f518e12729720a05ba6c18bd9b5b4435fa24713a
rodzikerg <131204207+rodzikerg@users.noreply.github.com>
2024-12-19 00:15:34 +0300

    Update daswq.py

--- daswq.py
+++ daswq.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter.messagebox import showinfo, showerror
+from tkinter import ttk
 
 auth_win = tk.Tk()
 auth_win.geometry('600x600')
@@ -22,7 +23,7 @@ def auth():
             auth_win.withdraw()
             admin_win.deiconify()
             return
-    showerror('ошибка','говно')
+    showerror('ошибка','вы не вошли')
 
 
 tk.Label(auth_win, text='логин').place(x=formx, y=formy)
@@ -36,7 +37,7 @@ password.place(x=formx-30, y=formy+80)
 tk.Button(auth_win, text='войти', command=auth).place(x=formx, y=formy+120)
 
 admin_win = tk.Tk()
-admin_win.geometry('1200x900+0+0')
+admin_win.geometry('1300x900+0+0')
 admin_win.withdraw()
 
 formax = 300
@@ -66,10 +67,18 @@ tk.Label(admin_win, text='ответственный').place(x=50, y=400)
 human = tk.Entry(admin_win, width=entry_width)
 human.place(x=50, y=430)
 
-tk.Label(admin_win, text='ответственный').place(x=50, y=480)
+tk.Label(admin_win, text='комментарий').place(x=50, y=480)
 comments = tk.Entry(admin_win, width=entry_width)
 comments.place(x=50, y=510)
 
+columns=['Имя клиента', 'Вид оргтехники', 'Модель', 'Описание проблемы', 'номер телефона', 'ответственный', 'комментарий']
+
+tree = ttk.Treeview(admin_win, show='headings', height=11, columns=columns)
+
+for index, el in enumerate(columns):
+    tree.column(index, width=150)
+    tree.heading(el, text=el)
+tree.place(x=200, y=10)
 tk.Button(admin_win, text='сохранить').place(x=formx, y=600)
 
 tk.Button(admin_win, text='Взять в работу').place(x=formx, y=650)
